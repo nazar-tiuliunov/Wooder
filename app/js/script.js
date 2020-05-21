df7c28e022dc70c089fc7a24dbf7abd2d23a37d6
Nazar Tiuliunov <tulunovn@gmail.com>
2020-05-22 00:45:36 +0300

    Work on background adaptabily.Added some elements in .header

--- app/js/script.js
+++ app/js/script.js
@@ -2,4 +2,7 @@ const header = document.getElementById("header")
 
 window.addEventListener("scroll", function(event) {
 	header.classList.add("active");
+	if (window.pageYOffset == 0) {
+		header.classList.remove("active");
+	}
 });
