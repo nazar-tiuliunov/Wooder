737c6c6cbfc4b5b7982c76e835b66f6a11ed702e
Nazar Tiuliunov <tulunovn@gmail.com>
2020-05-23 01:37:27 +0300

    .

--- app/js/script.js
+++
@@ -1,8 +0,0 @@
-const header = document.getElementById("header")
-
-window.addEventListener("scroll", function(event) {
-	header.classList.add("active");
-	if (window.pageYOffset == 0) {
-		header.classList.remove("active");
-	}
-});
